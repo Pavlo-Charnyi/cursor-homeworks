6c627789e25b5f969ddc4392343446c895860130
pavlo-rig <pavlo.charnyi+rig@gmail.com>
2022-09-19 13:57:30 +0300

    HW#9 done

---
+++ 09-homework/09-homework.js
@@ -0,0 +1,40 @@
+const generateBlocks = () => {
+  let container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container)
+
+  let divContainer = document.querySelector(".container");
+  for (let i = 0; i < 25; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = generateColor();
+    div.style.width = "50px";
+    div.style.height = "50px";
+    divContainer.append(div);
+  }
+}
+
+generateBlocks();
+
+const generateBlocksInterval = () => {
+  setInterval(() => {
+    const isContainer = document.querySelector(".container");
+    if(isContainer) {
+      isContainer.remove();
+    }
+    generateBlocks();
+  }, 1000)
+};
+
+generateBlocksInterval();
+
+//helper function
+function generateColor() {
+  const red = generateNumber();
+  const green = generateNumber();
+  const blue = generateNumber();
+  return `rgb(${red}, ${green}, ${blue})`;
+
+  function generateNumber() {
+    return Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+  }
+}
