cfc991be25fc6c7868f37a23d6529fe5e18d234a
pavlo-rig <pavlo.charnyi+rig@gmail.com>
2022-10-03 11:19:49 +0300

    HW#11 done

---
+++ 11-homework/11-homework.js
@@ -0,0 +1,37 @@
+const getRandomChinese = async (length) => {
+  function getChineseCharacter() {
+    const character = String.fromCharCode(Date.now().toString().slice(-5));
+    return character;
+  }
+
+  const promiseStartTime = performance.now() 
+  
+  let charactersString = "";
+
+  const delayedPromise = () => new Promise((resolve) => 
+      setTimeout(() => resolve(getChineseCharacter()), 50))
+  
+    while (length > 0) {
+      charactersString += await delayedPromise();
+      length--;
+    }
+
+  const promiseEndTime = performance.now()
+  const promiseWorkTime = Math.floor(promiseEndTime - promiseStartTime)
+  console.log(`Promise work time: ${promiseWorkTime} ms`);
+  
+  div = document.querySelector(".container")
+  div.textContent = charactersString;
+  h1 = document.createElement("h1")
+  h1.textContent = `Promise work time: ${promiseWorkTime} ms`;
+  div.append(h1)
+
+  // console.log(charactersString);
+  return charactersString;
+}
+
+getRandomChinese(10);
+
+//or get value like this:
+//getRandomChinese(10).then(value => console.log(value));
+
